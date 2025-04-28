8513d23ddec4fff2a17a8c8dc8d91df9e96b4def
arnegutschick <arne.gutschick@gmail.com>
2025-04-28 16:25:24 +0200

    Updated error page to handle different errors

--- error.php
+++ error.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-    <h1>Error 404: Ressource could not be found!</h1>
+<?php
+  if ($_GET['error'] == 'tooShort') {
+		echo "Error 400: Bitte geben Sie in der Suchleiste mindestens 3 Zeichen ein";
+	}
+
+	if ($_GET['error'] == 'invalidParam') {
+		echo "Error 400: Der übergebene Parameter ist nicht gültig";
+	}
+
+	if ($_GET['error'] == "missingParam") {
+		echo "Error 400: Bitte übergib einen gültigen Parameter";
+	}
+
+	if ($_GET['error'] == 'invalidID') {
+		echo "Error 404: Es konnte kein Künstler mit der angegebenen ID gefunden werden";
+	}
+?>
 </html>
