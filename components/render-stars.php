6b4fe42cea43e7ccf61d544e49054e715eb67da9
arnegutschick <arne.gutschick@gmail.com>
2025-06-15 16:35:16 +0200

    method for displaying rating as stars

---
+++ components/render-stars.php
@@ -0,0 +1,21 @@
+<?php
+function renderStars($rating) {
+    $stars = '';
+    $fullStars = floor($rating);
+    $halfStar = ($rating - $fullStars) >= 0.5;
+    $emptyStars = 5 - $fullStars - ($halfStar ? 1 : 0);
+
+    for ($i = 0; $i < $fullStars; $i++) {
+        $stars .= '★';
+    }
+    if ($halfStar) {
+        $stars .= '⯪';
+    }
+    for ($i = 0; $i < $emptyStars; $i++) {
+        $stars .= '☆';
+    }
+
+    return $stars;
+}
+
+?>
