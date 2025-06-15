9da246ef3704091303a124adde43c1d5a27ba830
arnegutschick <arne.gutschick@gmail.com>
2025-06-15 16:35:48 +0200

    most reviewed artists box on homepage

--- components/most-reviewed-artists.php
+++ components/most-reviewed-artists.php
@@ -3,30 +3,37 @@ require_once dirname(__DIR__)."/classes/Artist.php";
 require_once dirname(__DIR__)."/repositories/ArtistRepository.php";
 ?>
 
-<div class="card-deck">
-	<?php
+<div style="max-width: 500px; margin: auto;">
+  <div style="display: flex; gap: 10px; justify-content: center;">
+    <?php
+    $artistRepository = new ArtistRepository(new Database());
+    $mostReviewedArtists = $artistRepository->findMostReviewed(3);
+    ?>
 
-	$artistRepository = new ArtistRepository(new Database());
-	$mostReviewedArtists = $artistRepository->findMostReviewed(3);
-
-	foreach ($mostReviewedArtists as $index => $combined) {
-
-	    $artistName = $combined->getArtist()->getFirstName() . " " . $combined->getArtist()->getLastName();
-	    $reviewCount = $combined->getReviewCount();
-	    $position = $index + 1;
-
-	    echo "
-			<div class=\"card\">
-				<div class=\"card-body\">
-				<h4 class=\"card-title\">$position. $artistName</h4>
-				<div class=\"w-100\">
-					<p class=\"card-text\">
-					$reviewCount Reviews
-					</p>
-				</div>
-				</div>
-			</div>
-		";
-	}
-	?>
+    <?php foreach ($mostReviewedArtists as $index => $combined):
+      $artist = $combined->getArtist();
+      $artistName = $artist->getFirstName() . ' ' . $artist->getLastName();
+      $reviewCount = $combined->getReviewCount();
+      $artistId = $artist->getArtistId();
+      $position = $index + 1;
+      $imagePath = "/assets/images/artists/square-medium/" . $artistId . ".jpg";
+    ?>
+      <div class="col-md-4 mb-4">
+        <div class="card" style="width: 150px; min-height: 240px;">
+          <img class="card-img-top" style="height: 150px; width: 150px; object-fit: cover;" src="<?= $imagePath ?>" alt="">
+          <div class="card-body p-2">
+            <h4 class="card-title h6 mb-1">
+              <?= $position ?>.
+              <a href="artists/<?= $artistId ?>" style="color: black;">
+                <?= htmlspecialchars($artistName) ?>
+              </a>
+            </h4>
+            <p class="card-text small mb-0">
+              <?= $reviewCount ?> Reviews
+            </p>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
 </div>
