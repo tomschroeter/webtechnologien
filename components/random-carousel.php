ee1652e88953ae5b968b96aa107ee346b19d69c2
arnegutschick <arne.gutschick@gmail.com>
2025-06-15 16:55:56 +0200

    faster carousel

--- components/random-carousel.php
+++ components/random-carousel.php
@@ -52,11 +52,11 @@ $randomArtwork = $allArtworksAsLarge[array_rand($allArtworksAsLarge)];
   </div>
 
   <!-- Control buttons -->
-  <a class="carousel-control-prev" style="left: -70px; filter: invert(100%)" href="#homeCarousel" role="button" data-slide="prev" data-interval="2000">
+  <a class="carousel-control-prev" style="left: -70px; filter: invert(100%)" href="#homeCarousel" role="button" data-slide="prev" data-interval="500">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" style="right: -70px; filter: invert(100%)" href="#homeCarousel" role="button" data-slide="next" data-interval="2000">
+  <a class="carousel-control-next" style="right: -70px; filter: invert(100%)" href="#homeCarousel" role="button" data-slide="next" data-interval="500">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
