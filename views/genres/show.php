3a6ae7a8e5b6538e0f332e01b820c459489f3750
arnegutschick <arne.gutschick@gmail.com>
2025-06-16 16:38:44 +0200

    genre description

--- views/genres/show.php
+++ views/genres/show.php
@@ -1,14 +1,33 @@
 <br>
 <h1><?php echo htmlspecialchars($genre->getGenreName()) ?></h1>
 
+<div class="mt-3 ml-3">
+  <div class="row">
+    <div>
+      <?php
+      require_once dirname(dirname(__DIR__)) . "/components/find-image-ref.php";
+      $imagePath = "/assets/images/genres/square-medium/" . $genre->getGenreId() . ".jpg";
+      $placeholderPath = "/assets/placeholder/genres/square-medium/placeholder.svg";
+      $correctImagePath = getImagePathOrPlaceholder($imagePath, $placeholderPath);
+      ?>
+      <img src="<?php echo $correctImagePath ?>" alt="<?php echo htmlspecialchars($genre->getGenreName()) ?>"
+        style="height: 200px">
+    </div>
+    <div class="col">
+      <p><?php echo $genre->getDescription() ?></p>
+      <p>More Infos: <a href=<?php echo $genre->getLink() ?> target="_blank" class="text-decoration-none">Wikipedia</a>
+      </p>
+    </div>
+  </div>
+</div>
 <div class="mt-4">
   <div class="row">
     <div class="col-md-12">
       <h3>Artworks in this Genre</h3>
-      
+
       <?php if (!empty($artworks)): ?>
         <div class="row mt-4">
-          <?php 
+          <?php
           require_once dirname(dirname(__DIR__)) . "/components/artwork-card-list.php";
           renderArtworkCardList($artworks);
           ?>
