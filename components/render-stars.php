ae7d11518a6ee96b240de0cf03988e2f53cc1d84
tschroter <tschroter@tesla.com>
2025-06-18 20:28:27 +0200

    Render half stars using full stars

--- components/render-stars.php
+++ components/render-stars.php
@@ -1,21 +1,31 @@
 <?php
 function renderStars($rating): string
 {
-    $stars = '';
     $fullStars = floor($rating);
     $halfStar = ($rating - $fullStars) >= 0.5;
     $emptyStars = 5 - $fullStars - ($halfStar ? 1 : 0);
 
+    // Not using unicode half star (⯪), as half star isn't rendered on MacOs Chrome
+    $stars = '<span style="font-size: 0.9rem; vertical-align: 1px; user-select: none;">';
+    // Full stars
     for ($i = 0; $i < $fullStars; $i++) {
-        $stars .= '★';
-    }
-    if ($halfStar) {
-        $stars .= '⯪';
-    }
-    for ($i = 0; $i < $emptyStars; $i++) {
-        $stars .= '☆';
+        $stars .= '<span style="color: gold;">★</span>';
     }
 
+    // Half star
+    if ($halfStar) {
+        $stars .= '<span style="color: gold; position: relative; display: inline-block; width: 1em;">
+            <span style="position: absolute; overflow: hidden; width: 0.5em;">★</span>
+            <span style="color: #e4e5e9">★</span>
+        </span>';
+    }
+
+    // Empty stars
+    for ($i = 0; $i < $emptyStars; $i++) {
+        $stars .= '<span style="color: #e4e5e9;">★</span>';
+    }
+
+    $stars .= '</span>';
     return $stars;
 }
 ?>
