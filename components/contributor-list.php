6a4507df9ec83240bef8ba1a2d2e2991be1c5e51
arnegutschick <arne.gutschick@gmail.com>
2025-06-18 23:32:28 +0200

    contributor list

--- components/contributor-list.php
+++ components/contributor-list.php
@@ -1,9 +1,39 @@
 <?php
 $contributors = [
-  ['Arne Gutschick', 'arnegutschick', 'https://github.com/arnegutschick', 'https://avatars.githubusercontent.com/u/189605895?v=4'],
-  ['Kian van der Meer', 'karastift', 'https://github.com/karastift', 'https://avatars.githubusercontent.com/u/69339287?v=4'],
-  ['Tom Schröter', 'tomschroeter', 'https://github.com/tomschroeter', 'https://avatars.githubusercontent.com/u/151855431?v=4'],
-  ['Tim Fuchs', 'FixFaxt', 'https://github.com/FixFaxt', 'https://avatars.githubusercontent.com/u/75135582?v=4'],
-  ['Carlos Slaiwa', 'Carlos65l', 'https://github.com/Carlos65l', 'https://avatars.githubusercontent.com/u/146827754?v=4'],
-]
-  ?>
+  [
+    'name' => 'Arne Gutschick',
+    'githubUsername' => 'arnegutschick',
+    'githubUrl' => 'https://github.com/arnegutschick',
+    'profilePicture' => 'https://avatars.githubusercontent.com/u/189605895?v=4',
+    'tickets' => ['05 - Browse Artists', '09 - Simple Search', '10 - Search Results', '11 - Display Single Artist', '15 - Missing Images']
+  ],
+  [
+    'name' => 'Kian van der Meer',
+    'githubUsername' => 'karastift',
+    'githubUrl' => 'https://github.com/karastift',
+    'profilePicture' => 'https://avatars.githubusercontent.com/u/69339287?v=4',
+    'tickets' => ['06 - Browse Artworks', '12 - Display Single Artwork', '18 - Add To Favorites List', '19 - View Favorites List']
+  ],
+  [
+    'name' => 'Tom Schröter',
+    'githubUsername' => 'tomschroeter',
+    'githubUrl' => 'https://github.com/tomschroeter',
+    'profilePicture' => 'https://avatars.githubusercontent.com/u/151855431?v=4',
+    'tickets' => ['04 - About Us', '07 - Browse Genres', '08 - Browse Subjects', '14 - Display Single Subject', '23 - My Account', '25 - Map to Museum']
+  ],
+  [
+    'name' => 'Tim Fuchs',
+    'githubUsername' => 'FixFaxt',
+    'githubUrl' => 'https://github.com/FixFaxt',
+    'profilePicture' => 'https://avatars.githubusercontent.com/u/75135582?v=4',
+    'tickets' => ['02 - Home Page', '03 - Navigation', '13 - Display Single Genre', '24 - Advanced Search']
+  ],
+  [
+    'name' => 'Carlos Slaiwa',
+    'githubUsername' => 'Carlos65l',
+    'githubUrl' => 'https://github.com/Carlos65l',
+    'profilePicture' => 'https://avatars.githubusercontent.com/u/146827754?v=4',
+    'tickets' => ['16 - Add A Review', '17 - Delete A Review', '20 - Register User', '21 - Manage Users', '22 - Login as user']
+  ],
+];
+?>
