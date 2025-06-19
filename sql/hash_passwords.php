3ae3a31fea11f1389dc551e45ddb7b3716ec0e07
tschroter <tschroter@tesla.com>
2025-06-19 21:47:41 +0200

    Better comments password hashing script

--- sql/hash_passwords.php
+++ sql/hash_passwords.php
@@ -3,6 +3,7 @@
 * This script is used to drop the 'Salt' column in the database,
 * and to hash all passwords from default users provided by the Art_Database SQL script 
 * To use this script, navigate to 'USER/xampp/htdocs/src/sql' and run 'C:\xampp\php\php.exe hash_passwords.php'
+* MacOS: In src directory, you can run it with '/Applications/XAMPP/bin/php sql/hash_passwords.php '
 */
 
 // Load env vars
