2964df3ca8e326b510e1270a66f91fdd371caae9
arnegutschick <arne.gutschick@gmail.com>
2025-06-19 13:15:10 +0200

    formatting

--- views/artists/index.php
+++ views/artists/index.php
@@ -1,33 +1,33 @@
 <?php
 require_once dirname(dirname(__DIR__)) . "/components/find-image-ref.php";
 ?>
-<!-- Form providing the ability to sort the order of displayed artists -->
-<div class="d-flex align-items-center mt-4 mb-3">
-  <h1 class="flex-grow-1 mb-0">Artists</h1>
-  <form method="get">
-    <select name="sort" onchange="this.form.submit()" class="form-select">
-      <option value="ascending" <?php echo !$sort ? 'selected' : ''?>>Name (ascending)</option>
-      <option value="descending" <?php echo $sort ? 'selected' : ''?>>Name (descending)</option>
+
+<h1 class="mb-0 mt-4">Artists</h1>
+<div class="d-flex align-items-center justify-content-between mt-3 mb-3">
+  <p class="text-muted mb-0">Found: <?= count($artists) ?> artists</p>
+  <form method="get" class="w-auto ms-3">
+    <select name="sort" onchange="this.form.submit()" class="form-select form-select-sm">
+      <option value="ascending" <?= !$sort ? 'selected' : '' ?>>Name (ascending)</option>
+      <option value="descending" <?= $sort ? 'selected' : '' ?>>Name (descending)</option>
     </select>
   </form>
 </div>
-<p class="text-muted">Found: <?php echo count($artists)?> artists</p>
 
-<!-- List to display all artists -->
 <ul class="list-group mb-5">
   <?php if (!empty($artists)): ?>
     <?php foreach ($artists as $artist): ?>
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="/artists/<?php echo $artist->getArtistId()?>"
-           class="d-flex justify-content-between align-items-center w-100 text-decoration-none text-dark">
-          <span><?php echo htmlspecialchars($artist->getFirstName())?> <?php echo htmlspecialchars($artist->getLastName())?></span>
-          <!-- Checks if artists' image exists -->
-          <?php
-  $imagePath =  "/assets/images/artists/square-thumb/".$artist->getArtistId().".jpg";
-  $placeholderPath = "/assets/placeholder/artists/square-thumb/placeholder.svg";
-  $correctImagePath = getImagePathOrPlaceholder($imagePath, $placeholderPath);
-  ?>
-          <img src="<?php echo $correctImagePath?>" alt="Picture of Artist" style="max-width: 100px; max-height: 100px; object-fit: cover;">
+      <?php
+      $imagePath = "/assets/images/artists/square-thumb/" . $artist->getArtistId() . ".jpg";
+      $placeholderPath = "/assets/placeholder/artists/square-thumb/placeholder.svg";
+      $correctImagePath = getImagePathOrPlaceholder($imagePath, $placeholderPath);
+      ?>
+      <li class="list-group-item p-2">
+        <a href="/artists/<?= $artist->getArtistId() ?>"
+          class="d-flex justify-content-between align-items-center text-dark w-100 link-underline-on-hover"
+          style="padding-left: 1rem; padding-right: 1rem;">
+          <span><?= htmlspecialchars($artist->getFirstName() . ' ' . $artist->getLastName()) ?></span>
+          <img src="<?= $correctImagePath ?>" alt="Picture of Artist"
+            style="max-width: 100px; max-height: 100px; object-fit: cover; margin-left: 1rem;">
         </a>
       </li>
     <?php endforeach; ?>
